134c13803156a1ff99fa59125e0a2dec9d475166
Vlad Ageysha <ander123fakk@gmail.com>
2022-11-20 20:36:19 +0300

    Draw circle

--- script.js
+++ script.js
@@ -5,22 +5,34 @@ cvs.addEventListener('click', async (e) => {
     const x = e.pageX - cvs.offsetLeft - 25;
     const y = e.pageY - cvs.offsetTop - 25;
 
-    await animationLine(x, y, x + 50, y + 50, 5);
-    await animationLine(x + 50, y, x, y + 50, 5);
+    // await animationLine(x, y, x+50, y, 5);
+    // await animationLine(x+50, y, x+50, y+50, 5);
+    // await animationLine(x+50, y+50, x, y+50, 5);
+    // await animationLine(x, y+50, x, y, 5);
+
+    ctx.arc(250, 250, 100, 0, (Math.PI/180)*50);
+    ctx.stroke();
+
+    // await animationLine(x, y, x+50, y+50, 5);
+    // await animationLine(x+50, y, x, y+50, 5);
 });
 
 function animationLine(x1, y1, x2, y2, speed = 1) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         
         function animationStep(x1, y1, x2, y2, speed) {
             ctx.beginPath();
             ctx.moveTo(x1, y1);
 
-            if (x1 > x2) x1 -= speed;
-            else x1 += speed;
+            if (x1 !== x2) {
+                if (x1 > x2) x1 -= speed;
+                else x1 += speed;
+            }
 
-            if (y1 > y2) y1 -= speed;
-            else y1 += speed;
+            if (y1 !== y2) {
+                if (y1 > y2) y1 -= speed;
+                else y1 += speed;
+            }
 
             ctx.lineTo(x1, y1);
             ctx.stroke();
