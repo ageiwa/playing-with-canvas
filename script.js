91922a7235cc641bd8a79a0d79d929928a56bc27
Vlad Ageysha <ander123fakk@gmail.com>
2022-11-20 19:26:45 +0300

    Add promise for step by step animation with await

--- script.js
+++ script.js
@@ -5,62 +5,48 @@ cvs.addEventListener('click', async (e) => {
     const x = e.pageX - cvs.offsetLeft - 25;
     const y = e.pageY - cvs.offsetTop - 25;
 
-    animationLine(x, y, x + 50, y + 50, 2);
-    animationLine(x + 50, y, x, y + 50, 2);
+    await animationLine(x, y, x + 50, y + 50, 5);
+    await animationLine(x + 50, y, x, y + 50, 5);
 });
 
-function animationLine(x1, y1, x2, y2, speed) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
+function animationLine(x1, y1, x2, y2, speed = 1) {
+    return new Promise((resolve, reject) => {
+        
+        function animationStep(x1, y1, x2, y2, speed) {
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
 
-    if (x1 > x2) x1 -= speed;
-    else x1 += speed;
+            if (x1 > x2) x1 -= speed;
+            else x1 += speed;
 
-    if (y1 > y2) y1 -= speed;
-    else y1 += speed;
+            if (y1 > y2) y1 -= speed;
+            else y1 += speed;
 
-    ctx.lineTo(x1, y1);
-    ctx.stroke();
+            ctx.lineTo(x1, y1);
+            ctx.stroke();
 
-    if (x1 === x2 && y1 === y2) return;
+            if (x1 === x2 && y1 === y2) resolve();
+            else requestAnimationFrame(() => animationStep(x1, y1, x2, y2, speed));
+        }
 
-    requestAnimationFrame(() => animationLine(x1, y1, x2, y2, speed));
+        requestAnimationFrame(() => animationStep(x1, y1, x2, y2, speed));
+    });
 }
 
-// function animationDrawX(x, y, point, step = 1) {
-//     if (step === 1) {
-//         if (x < point[0] && y < point[1]) {
-//             ctx.beginPath();
-//             ctx.moveTo(x, y);
-    
-//             x += 5;
-//             y += 5;
-    
-//             ctx.lineTo(x, y);
-//             ctx.stroke();
-//         }
-//         else {
-//             step = 2;
+// function animationLine(x1, y1, x2, y2, speed) {
+//     ctx.beginPath();
+//     ctx.moveTo(x1, y1);
 
-//             y -= 50;
-//             point = [x-50, y+50];
-//         }
-//     }
-//     else if (step === 2) {
-//         if (x > point[0] && y < point[1]) {
+//     if (x1 > x2) x1 -= speed;
+//     else x1 += speed;
 
-//             ctx.beginPath();
-//             ctx.moveTo(x, y);
-    
-//             x -= 5;
-//             y += 5;
-    
-//             ctx.lineTo(x, y);
-//             ctx.stroke();
-//         }
-//         else step = null;
-//     }
-//     else return;
+//     if (y1 > y2) y1 -= speed;
+//     else y1 += speed;
 
-//     requestAnimationFrame(() => animationDrawX(x, y, point, step));
+//     ctx.lineTo(x1, y1);
+//     ctx.stroke();
+
+//     if (x1 === x2 && y1 === y2) return new Promise((resolve) => resolve('Готово!'));
+
+//     requestAnimationFrame(() => animationLine(x1, y1, x2, y2, speed));
 // }
